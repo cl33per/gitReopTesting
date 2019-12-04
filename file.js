f2ac97c824f1a01fc0327fae3987e36646117350
cl33per <leeper08@gmail.com>
2019-12-03 21:11:21 -0500

    yup

--- file.js
+++ file.js
@@ -1,6 +1,8 @@
 const testing = [1,2,3,4,5,6] 
 const testingFunction = function(testing){
-    // console.log(testing);
+    // console.log(testinasdg);
     return testing
 };
-testingFunction(1);
+
+
+testingFunction();
